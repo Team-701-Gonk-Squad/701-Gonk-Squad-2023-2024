7ae07b5c825c0e5a310d7899250fd500cfb82abd
StephenBalcirak <103044196+SpacePianist0@users.noreply.github.com>
2023-09-16 10:55:29 -0400

    hewlp mwe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/chassis.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class chassis {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+@Disabled
+public class chassis extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        telemetry.addData("weeboo : ", "weeboo");
+        telemetry.update();
+    }
 }
